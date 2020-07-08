7d54deee97ad3cdeed9fbb4c67c87e491c46cfcd
Sylvain Périssé <s.perisse@eurelis.com>
2020-07-08 18:20:19 +0200

    remove error for double member

--- components/projectForm/steps/Contact.vue
+++ components/projectForm/steps/Contact.vue
@@ -182,11 +182,6 @@
               Add Member
             </v-btn>
           </v-col>
-          <v-col
-            cols="12"
-          >
-            <span>Erreur</span>
-          </v-col>
         </v-row>
         <v-list
           v-if="team && team.length"
